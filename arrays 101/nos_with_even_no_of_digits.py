c0e68cb26eef34446cab9bc297d85fbabdfef3ac
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-07-16 11:06:58 +0530

    Find numbers with even number of digits

---
+++ arrays 101/nos_with_even_no_of_digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for n in nums:
+            digit=0
+            while(n!=0):
+                n=n//10
+                digit+=1
+            if digit%2==0:
+                count+=1
+        return count
+                
