f8210c456edba44606198e6d127a7f8f17bf3f79
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-07-16 11:09:41 +0530

    Find numbers with even number of digits in java

---
+++ arrays 101/nos_with_even_no_of_digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0, i;
+        for(i=0; i<nums.length; i++)
+        {
+            int digit=0;
+            while(nums[i]!=0)
+            {
+                nums[i] = nums[i]/10;
+                digit++;
+            }
+            
+            if(digit % 2 == 0)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
