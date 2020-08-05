9518abd6e52b3c831c67698b92c1a7cfd9347502
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-08-05 23:09:51 +0530

    Updated Squares of a Sorted Array in java

--- arrays 101/squares_of_a_sorted_array.java
+++ arrays 101/squares_of_a_sorted_array.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 class Solution {
     public int[] sortedSquares(int[] A) {
         
@@ -8,18 +9,8 @@ class Solution {
             A[i] = A[i]*A[i];
         }
         
-        for(i=0; i<A.length; i++)
-        {
-            for(int j=0; j<A.length-1; j++)
-            {
-                if(A[j]>A[j+1])
-                {
-                    temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;
-                }
-            }
-        }
+        
+        Arrays.sort(A);
         
         return A;
     }
