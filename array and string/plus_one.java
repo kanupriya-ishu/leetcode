18b7ffcd0652feec37191cf2be9972770e30c438
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-08-23 16:22:02 +0530

    Optimised Plus One in java

--- array and string/plus_one.java
+++ array and string/plus_one.java
@@ -1,36 +1,23 @@
-import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
-        BigInteger n = new BigInteger("0");
-		BigInteger A;
-        for(int i=0; i<digits.length; i++)
-        {
-        	A  = BigInteger.valueOf(digits[i]);
-        	n = n.multiply(BigInteger.valueOf(10));
-        	n = n.add(A);
+    for (int i = digits.length - 1; i >=0; i--) {
+        if (digits[i] != 9) {
+            digits[i]++;
+            break;
+        } else {
+            digits[i] = 0;
         }
-        
-        n = n.add(BigInteger.valueOf(1));
-        int digi = getDigitCount(n);
-        int[] arr = new int[digi];
-        for(int i =digi-1; i>=0; i--)
-        {
-        	BigInteger temp = n.mod(BigInteger.valueOf(10));
-        	n = n.divide(BigInteger.valueOf(10));
-        	arr[i] = temp.intValue();
-        }
-        
-        
-        return arr;
-            
     }
-    
-    public static int getDigitCount(BigInteger number) {
-		  double factor = Math.log(2) / Math.log(10);
-		  int digitCount = (int) (factor * number.bitLength() + 1);
-		  if (BigInteger.TEN.pow(digitCount - 1).compareTo(number) > 0) {
-		    return digitCount - 1;
-		  }
-		  return digitCount;
-		}
+        
+    if(digits[0]==0)
+    {
+        int[] arr = new int[digits.length+1];
+        arr[0] = 1;
+        return arr;
+    }
+        
+        return digits;
+        
+        
+    }
 }
