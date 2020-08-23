9c4b7bd33d6acafae24de915f139e95296c36e9a
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-08-23 16:26:24 +0530

    Optimised Plus One in python

--- array and string/plus_one.py
+++ array and string/plus_one.py
@@ -1,15 +1,9 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]!=9:
-                digits[i]+=1
-                break
-            else:
-                digits[i]=0
-        l=[]
-        if digits[0]==0:
-            l.append(1)
-            l=l+digits
-            return l
+        s = ""
+        for i in digits:
+            s += str(i)
         
-        return digits
+        s = str(int(s) + 1)
+        
+        return [i for i in s]
