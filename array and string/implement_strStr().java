570a388dfbf78755f9c31380f68ddd686dadea8d
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-08-25 15:50:08 +0530

    Implement strStr() in java

--- array and string/implement_strStr().java
+++ array and string/implement_strStr().java
@@ -1,7 +1,24 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        if len(needle)==0:
-            return 0
-        if needle in haystack:
-            return haystack.find(needle)
-        return -1
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if (needle.length()==0)
+            return 0;
+       
+        for(int i=0; i<=(haystack.length()-needle.length()); i++)
+        {
+            int j;
+            
+            for(j=0; j<needle.length(); j++)
+            {
+                if(haystack.charAt(i+j)!=needle.charAt(j))
+                    break;
+            }
+            
+            if(j==needle.length())
+                return i;
+        }
+        
+        return -1;
+        
+    }
+}
