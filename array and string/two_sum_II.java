878412a28307d8f7778d6c880c36b7bfe583ef26
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-09-04 11:59:02 +0530

    Optimised Two Sum II in java

--- array and string/two_sum_II.java
+++ array and string/two_sum_II.java
@@ -1,19 +1,29 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] arr = new int[2];
         
-        for(int i=0; i<numbers.length; i++)
+        int l=0;
+        int r=numbers.length-1;
+        
+        while(l<r)
         {
-            for(int j=i+1; j<numbers.length; j++)
+            if(target > numbers[l] + numbers[r])
             {
-                if(numbers[i]+numbers[j]==target)
-                {
-                    arr[0] = i+1;
-                    arr[1] = j+1;
-                    break;
-                }
+                l++;
+            }
+            
+            else if(target < numbers[l] + numbers[r])
+            {
+                r--;
+            }
+            else{
+                break;
             }
         }
+        
+        int[] arr = new int[2];
+        
+        arr[0] = l+1;
+        arr[1] = r+1;
         
         return arr;
     }
