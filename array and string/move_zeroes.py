dc09ad69420b1bffedc33ce246c1cd4f5a2f478e
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-09-14 16:43:41 +0530

    Move Zeroes in python

---
+++ array and string/move_zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[length] = nums[i]
+                length += 1
+        
+        for i in range(length,len(nums)):
+            nums[i] = 0
+            
