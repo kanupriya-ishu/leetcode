0f39afb18e6da3058daef2a356dab468309b9075
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-10-09 11:10:09 +0530

    Optimised program: Time efficient

--- arrays 101/duplicate_zeros.java
+++ arrays 101/duplicate_zeros.java
@@ -1,14 +1,24 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-			if(arr[i]==0) {
-				for (int j = arr.length-1; j >i; j--) {
-					arr[j]=arr[j-1];
-				}
-				i++;
-			}
-			
+        int[] ans = new int[arr.length];
+        int len = 0; 
+        for (int i = 0; i < arr.length && len<arr.length; i++) {
+			if (arr[i]!=0)
+            {
+                ans[len++]=arr[i];
+            }
+            else
+            {
+                ans[len++]=0;
+                if(len<arr.length)
+                 ans[len++]=0;
+            }
 		}
+        
+        for(int i=0; i<ans.length; i++)
+        {
+            arr[i] = ans[i];
+        }
         
     }
 }
